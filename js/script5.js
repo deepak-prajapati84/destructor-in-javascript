5504c327963fee3c6faa708ce3c09ddc2cdbea92
Deepak kumar <dkp0043@gmail.com>
2023-08-28 00:12:48 +0530

    js_object

--- js/script5.js
+++ js/script5.js
@@ -32,3 +32,41 @@ console.log(student);
 
 console.log(student.teacher[0]);
 console.log(student.teacher[0].name);
+
+
+let y={
+    name:"y",
+    surname:'yy',
+    namea:"Deepak Prajapati",
+    "friends":["adeoa",'daml','eofjwe','dlfsdp'],
+
+    myfuncA:function(){
+        return this.name;
+    },
+
+    myfunc:()=>{
+        return this.namea;
+    },
+    
+}
+
+console.log(y);
+console.log(y.myfuncA());
+console.log(y.myfunc());
+
+/*
+    for(expression;condition,increment/decrements){
+        //code here
+    }
+*/
+
+for(let i=0;i<y.friends.length;i++){
+    console.log(y.friends[i]);
+}
+
+//foreach loop
+let yy=['a','d','dekld',"xm;sa","slmda;s"];
+
+yy.forEach(element => {
+    console.log(element);
+});
